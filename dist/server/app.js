dd88de696571acbc9ce76a57a0e00b191fa4821f
Fabricio Loupias <loupias.fabri@gmail.com>
2018-08-02 14:35:35 -0300

    Push heroku

---
+++ dist/server/app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var cors = require('cors')
+var app = express();
+
+
+//Settings
+app.set('port', process.env.PORT || 3000);
+
+//Middlewares
+app.use(express.json());
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://valor-software.github.io');
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+app.use(cors({origin: 'http://localhost:4200'}))
+
+//Routes
+app.use('/', require('./routes/api-spotify.route'));  
+app.use(express.static(__dirname + '/dist/falou-app'))
+
+//Start server
+app.get('*', function(req, res){
+    res.send('hello world');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running`);
+  });
